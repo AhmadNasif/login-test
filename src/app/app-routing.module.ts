1c05e5eb0ef9c80515d74ad78c67b3a237383f32
AhmadNasif <1ahmad.nasif@gmail.com>
2024-08-06 18:14:13 +0300

    Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login-page', pathMatch: 'full' },
+  {
+    path: 'login-page',
+    loadChildren: () => import('./pages/login-page/login-page.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  { path: '**', redirectTo: '/login-page' }// waild card if the user write wrong url
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
